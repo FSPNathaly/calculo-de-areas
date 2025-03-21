88adce2eaf43531f4e5e5f511a82a96f5eb7b84d
Nathaly Ferreira <104204488+FSPNathaly@users.noreply.github.com>
2025-03-21 11:06:18 -0300

    Update Main.java

--- calculo-de-areas-java/src/main/java/Main.java
+++ calculo-de-areas-java/src/main/java/Main.java
@@ -4,37 +4,46 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        double opcao;
+        int opcao;
 
-        System.out.print("1 - Quadrado\n 2 - Circulo\n 3 - Triângulo\n 0 - Fim\n Digite a opção: ");
-        opcao = ler.nextDouble();
+        do {
+            System.out.println("1 - Quadrado\n2 - Círculo\n3 - Triângulo\n0 - Sair");
+            System.out.print("Digite a opção: ");
+            opcao = ler.nextInt();
 
-        while (opcao != 0) {
-            if (opcao == 1) {
-                System.out.print("Digite o lado do quadrado: ");
-                double lado = ler.nextDouble();
-                double quadrado = lado * lado;
-                System.out.println("O resultado é: " + quadrado);
-            } else if (opcao == 2) {
-                System.out.print("Digite o raio do circulo: ");
-                double raio = ler.nextDouble();
-                double circulo = 3.14 * raio * raio;
-                System.out.println("O resultado é: " + circulo);
-            } else if (opcao == 3) {
-                System.out.println("Digite a base do triângulo:");
-                double base = ler.nextDouble();
-                System.out.println("Digite a altura do triângulo: ");
-                double altura = ler.nextDouble();
-                double triangulo = (base * altura);
-                System.out.println("O resultado é: " + triangulo);
-            } else if (opcao == 0) {
-                System.out.println("Fim");
-            } else {
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o lado do quadrado: ");
+                    double lado = ler.nextDouble();
+                    double areaQuadrado = lado * lado;
+                    System.out.println("Área do quadrado: " + areaQuadrado);
+                    break;
+
+                case 2:
+                    System.out.print("Digite o raio do círculo: ");
+                    double raio = ler.nextDouble();
+                    double areaCirculo = Math.PI * raio * raio;
+                    System.out.println("Área do círculo: " + areaCirculo);
+                    break;
+
+                case 3:
+                    System.out.print("Digite a base do triângulo: ");
+                    double base = ler.nextDouble();
+                    System.out.print("Digite a altura do triângulo: ");
+                    double altura = ler.nextDouble();
+                    double areaTriangulo = (base * altura) / 2;
+                    System.out.println("Área do triângulo: " + areaTriangulo);
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
             }
-            System.out.println("1 - Quadrado\n 2 - Circulo\n 3 - Triângulo\n 0 - Fim\n Digite a opção: ");
-            opcao = ler.nextDouble();
-        }
+        } while (opcao != 0);
+
         ler.close();
     }
 }
